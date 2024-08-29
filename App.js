ad4341741b908c852b1d6939ac6213c54b85377c
rowancape <niklasmk.pihlajaniemi@gmail.com>
2024-08-29 03:01:07 +0300

    Valmis!

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState()
+  const [range, setRange] = useState("Enter age")
+
+  const calculate = () => {
+    if (age > 0 && age <= 122) {
+      const val = 220 - age
+      setRange(Math.round( val*0.65 ) + "-" + Math.round( val*0.85 ))
+    }
+    else {
+      setRange("Suspicious age input...")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inline}>
+        <Text style={styles.age}>Age:</Text>
+        <TextInput 
+          style={styles.input}
+          value={age}
+          onChangeText={text => setAge(text)}
+          keyboardType='decimal-pad'
+        />
+      </View>
+      <Button title="Calculate" onPress={calculate}></Button>
+      <Text style={styles.resultText}>Heart rate range result:</Text>
+      <Text style={styles.result}>{range} BPM</Text>
     </View>
   );
 }
@@ -13,8 +37,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
   },
+  inline: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  age: {
+    marginBottom: 10,
+    fontSize: 20,
+  },
+  input: {
+    marginBottom: 10,
+    marginLeft: 10,
+    paddingLeft: 5,
+    height: 50,
+    width: 100,
+    fontSize: 20,
+    textAlign: 'center',
+    borderWidth: 3,
+    borderRadius: 5,
+  },
+  resultText: {
+    fontSize: 25,
+    marginTop: 35
+  },
+  result: {
+    marginBottom: 10,
+    color: "blue",
+    fontSize: 25,
+    fontWeight: '900'
+  }
 });
